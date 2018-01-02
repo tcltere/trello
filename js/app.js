5d6533de5bbf7a133035e89b0e57f97cb189144b
tcltere <t.canette@gmail.com>
2018-01-02 00:53:32 -0300

    subiendo trello correccion

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
-var mostrarLista = document.getElementById("list");
 
 
-mostrarLista.addEventListener("click", function(){
+document.getElementById("btnIngresar").addEventListener("click", add);
+function add(){
+	var remover = document.getElementById("btnIngresar"); 
+remover.style.display = 'none';
+    var aparecer = document.getElementById("Tabla1"); 
+aparecer.style.display = 'inline-block';
 
-	document.getElementById("lista").classList.add("after");
-	document.getElementById("buttons").classList.remove("lista");
-});
 
+}
+document.getElementById("btnSave").addEventListener("click", tablero);
+function tablero(){
+	var mensaje = document.getElementById("nombrelista").value;
+
+	document.getElementById("nombrelista").value = "";
+	var list = document.getElementById("list");
+   
+
+    var nodo = document.createTextNode(mensaje);
+    var parrafo = document.createElement("p");
+    var contenedor = document.createElement("div");
+    parrafo.appendChild(nodo);
+    contenedor.appendChild(parrafo);
+    list.appendChild(contenedor);
+
+}
